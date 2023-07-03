8b63a2f05c29d9ba299c93e727be381265b3ddbc
kaf-lamed-beyt <belac335@gmail.com>
2023-07-03 13:48:42 +0100

    feat: add support for Next.js

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Head from "next/head";
 
 interface metaProps {
   url: string;
   pageTitle: string;
   description: string;
+  next: React.ReactNode;
   children?: React.ReactNode;
   previewImage?: string;
   contentType?: string;
@@ -11,6 +13,7 @@ interface metaProps {
 
 export const MetaData = ({
   url,
+  next,
   children,
   pageTitle,
   description,
@@ -18,34 +21,72 @@ export const MetaData = ({
   previewImage,
 }: metaProps) => {
   return (
-    <head>
-      <title>{pageTitle}</title>
-      <meta name="description" content={description} />
+    <>
+      {next ? (
+        <Head>
+          <title>{pageTitle}</title>
+          <meta name="description" content={description} />
 
-      <meta itemProp="name" content={pageTitle} />
-      <meta itemProp="description" content={pageTitle} />
-      {previewImage ? <meta itemProp="image" content={previewImage} /> : null}
+          <meta itemProp="name" content={pageTitle} />
+          <meta itemProp="description" content={pageTitle} />
+          {previewImage ? (
+            <meta itemProp="image" content={previewImage} />
+          ) : null}
 
-      <meta property="og:url" content={url} />
-      <meta
-        property="og:type"
-        content={contentType ? contentType : "website"}
-      />
-      <meta property="og:title" content={pageTitle} />
-      <meta property="og:description" content={pageTitle} />
-      {previewImage ? (
-        <meta property="og:image" content={previewImage} />
-      ) : null}
+          <meta property="og:url" content={url} />
+          <meta
+            property="og:type"
+            content={contentType ? contentType : "website"}
+          />
+          <meta property="og:title" content={pageTitle} />
+          <meta property="og:description" content={pageTitle} />
+          {previewImage ? (
+            <meta property="og:image" content={previewImage} />
+          ) : null}
 
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={pageTitle} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:site" content={url} />
-      {previewImage ? (
-        <meta name="twitter:image" content={previewImage} />
-      ) : null}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content={pageTitle} />
+          <meta name="twitter:description" content={description} />
+          <meta name="twitter:site" content={url} />
+          {previewImage ? (
+            <meta name="twitter:image" content={previewImage} />
+          ) : null}
 
-      {children}
-    </head>
+          {children}
+        </Head>
+      ) : (
+        <head>
+          <title>{pageTitle}</title>
+          <meta name="description" content={description} />
+
+          <meta itemProp="name" content={pageTitle} />
+          <meta itemProp="description" content={pageTitle} />
+          {previewImage ? (
+            <meta itemProp="image" content={previewImage} />
+          ) : null}
+
+          <meta property="og:url" content={url} />
+          <meta
+            property="og:type"
+            content={contentType ? contentType : "website"}
+          />
+          <meta property="og:title" content={pageTitle} />
+          <meta property="og:description" content={pageTitle} />
+          {previewImage ? (
+            <meta property="og:image" content={previewImage} />
+          ) : null}
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content={pageTitle} />
+          <meta name="twitter:description" content={description} />
+          <meta name="twitter:site" content={url} />
+          {previewImage ? (
+            <meta name="twitter:image" content={previewImage} />
+          ) : null}
+
+          {children}
+        </head>
+      )}
+    </>
   );
 };
